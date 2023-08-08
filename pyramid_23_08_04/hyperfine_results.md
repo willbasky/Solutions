346bc8977ce5d44477b3bf77d4b8f5bc1a7ef000
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2023-08-08 00:38:30 -0300

    Rust: one more realization. Add hyperfine benchmarks

---
+++ pyramid_23_08_04/hyperfine_results.md
@@ -0,0 +1,4 @@
+| Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
+|:---|---:|---:|---:|---:|
+| `rust-pyramid (what_a_name2)` | 5.4 ± 1.9 | 2.6 | 10.6 | 1.00 |
+| `haskell-pyramid (wMVector)` | 55.3 ± 10.7 | 31.7 | 81.9 | 10.27 ± 4.06 |
--- pyramid_23_08_04/pyramid-rust/src/main.rs
+++ pyramid_23_08_04/pyramid-rust/src/main.rs
@@ -1,15 +1,20 @@
 fn main() {
-  let answer = what_a_name(322,156);
-  println!("top: 322 and left: 156 equal to {}", answer);
-  assert_eq!(what_a_name(322,156), 306.48749781747574);
-  assert_eq!(what_a_name(3,3), 0.875);
-  assert_eq!(what_a_name(0,0), 0.0)
+  // let answer = what_a_name(322,156);
+  // println!("what_a_name1: top: 322 and left: 156 equal to {}", answer);
+  let answer = what_a_name2(322,156);
+  println!("what_a_name2: top: 322 and left: 156 equal to {}", answer);
+  // let answer = what_a_name2(3,2);
+  // println!("top: 3 and left: 2 equal to {}", answer);
+  // assert_eq!(what_a_name(322,156), 306.48749781747574);
+  // assert_eq!(what_a_name2(322,156), 306.48749781747574);
+  // assert_eq!(what_a_name2(2,2), 0.75);
+  // assert_eq!(what_a_name(0,0), 0.0)
 }
 
 fn what_a_name (top: usize, left: usize) -> f64 {
+  if top == 0 { return 0.};
   let mut counter = top;
   let mut pyramid: Vec<Vec<f64>> = vec![vec![0., 1., 0.]];
-  if top == 0 { return 0.};
   while counter > 0 {
     if let Some(row) = pyramid.get(&pyramid.len() - 1) {
       let current_row = row;
@@ -30,5 +35,25 @@ fn what_a_name (top: usize, left: usize) -> f64 {
   }
 
   pyramid.as_slice()[top][left+1] - 1.
+
+}
+
+fn what_a_name2 (top: usize, left: usize) -> f64 {
+  if top == 0 { return 0.};
+  if top == 1 { return 0.5};
+  let mut row: Vec<f64> = vec![1.5,1.5,0.];
+  let mut buf: Vec<f64> = vec![0.;1];
+
+  for tx in 2..top+1 {
+    for ix in 0..tx+1 {
+      let element = row[ix];
+      let prev_element = buf[0];
+      let new_element = 1. + (element + prev_element) / 2.;
+      buf[0] = element;
+      row[ix] = new_element;
+    }
+    row.push(0.);
+  }
+  row.as_slice()[left] - 1.
 
 }
